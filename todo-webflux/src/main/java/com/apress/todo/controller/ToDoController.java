c97137468228ddb1ac410d5d9c6c70288f5a3e2f
rafael claumann bernardes <rafael_claumann@hotmail.com>
2021-05-25 20:29:22 -0300

    todo-webflux; ToDo functional controller

--- todo-webflux/src/main/java/com/apress/todo/controller/ToDoController.java
+++ todo-webflux/src/main/java/com/apress/todo/controller/ToDoController.java
@@ -1,30 +1,30 @@
-package com.apress.todo.controller;
-
-import com.apress.todo.domain.ToDo;
-import com.apress.todo.repository.ToDoRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-@RestController
-public class ToDoController {
-
-    private ToDoRepository toDoRepository;
-
-    public ToDoController(ToDoRepository toDoRepository) {
-        this.toDoRepository = toDoRepository;
-    }
-
-    @GetMapping("/todo/{id}")
-    public Mono<ToDo> getToDo(@PathVariable String id) {
-        return this.toDoRepository.findById(id);
-    }
-
-    @GetMapping("/todo")
-    public Flux<ToDo> getToDos() {
-        return this.toDoRepository.findAll();
-    }
-
-}
+//package com.apress.todo.controller;
+//
+//import com.apress.todo.domain.ToDo;
+//import com.apress.todo.repository.ToDoRepository;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RestController;
+//import reactor.core.publisher.Flux;
+//import reactor.core.publisher.Mono;
+//
+//@RestController
+//public class ToDoController {
+//
+//    private ToDoRepository toDoRepository;
+//
+//    public ToDoController(ToDoRepository toDoRepository) {
+//        this.toDoRepository = toDoRepository;
+//    }
+//
+//    @GetMapping("/todo/{id}")
+//    public Mono<ToDo> getToDo(@PathVariable String id) {
+//        return this.toDoRepository.findById(id);
+//    }
+//
+//    @GetMapping("/todo")
+//    public Flux<ToDo> getToDos() {
+//        return this.toDoRepository.findAll();
+//    }
+//
+//}
